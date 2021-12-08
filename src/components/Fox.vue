590c3040df752eb0f07b043220a537df200eaded
josebGithub <46608139+josebGithub@users.noreply.github.com>
2021-12-07 20:40:06 -0800

    Initial commit

---
+++ src/components/Fox.vue
@@ -0,0 +1,19 @@
+<template>
+  <span
+    ><img :src="fox" class="favorites-img" />
+    <button @click="$emit('remove')" class="remove">Remove</button>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    fox: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
